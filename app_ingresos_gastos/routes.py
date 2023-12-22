f2fa3342770f06b86a22684af64cbd76580d31c9
LucasTorreg <lucastorregrosa@gmail.com>
2023-12-22 12:51:38 +0100

    validación de fechas en método new, guardar Post de formulario en csv con librería csv

--- app_ingresos_gastos/routes.py
+++ app_ingresos_gastos/routes.py
@@ -1,24 +1,44 @@
 from app_ingresos_gastos import app
-from flask import render_template
+from flask import render_template, request, redirect
+import csv
+from datetime import date
 
 @app.route("/")
 def index():
-    datos = [
-        {"fecha":"01/03/2023",
-        "concepto":"Salario",
-        "monto":"1500"},
-        {"fecha":"01/05/2023",
-        "concepto":"Ropa",
-        "monto":"-150"},
-        {"fecha":"01/10/2023",
-        "concepto":"Supermercado",
-        "monto":"-200"}
-    ]
+    datos = []
+    #llamada al archivo csv
+    fichero = open('data/movimientos.csv','r')
+    #accediendo a cada registro de archivo y darle formato
+    csvReader = csv.reader(fichero,delimiter=",",quotechar='"')
+    for items in csvReader:
+        datos.append(items)
+    
+
     return render_template("index.html", data = datos, titulo = "Lista")
 
-@app.route("/new")
+@app.route("/new", methods=["GET", "POST"])
 def new():
-    return render_template("new.html", titulo = "Nuevo", tipoAccion = "Registro", tipoBoton = "Guardar")
+    if request.method== "POST":
+        hoy = str(date.today())#esto da la fecha de hoy
+
+        if request.form["fecha"] > hoy:
+            return render_template("new.html",titulo="Nuevo",tipoAccion="Registro",tipoBoton="Guardar")
+
+        if request.form["fecha"] <= hoy:
+            #return "esto deberia registrarse " + str(request.form)
+            #acceder al archivo y configurar para la carga de un nuevo registro
+            mifichero = open('data/movimientos.csv','a',newline='')
+            #llamar al método writer de escritura y configuramos formato
+            lectura = csv.writer(mifichero,delimiter=',', quotechar='"')
+            #registramos los datos recibidos en el archivo csv
+            lectura.writerow([ request.form["fecha"], request.form["concepto"], request.form["monto"] ])
+            mifichero.close()
+
+            return redirect("/")
+
+    else:#si es GET
+        return render_template("new.html",titulo="Nuevo",tipoAccion="Registro",tipoBoton="Guardar")
+
 
 @app.route("/delete")
 def delete():
--- pruebas.py
+++ pruebas.py
@@ -11,7 +11,7 @@ with open("data/movimientos.csv","r") as resultado:
 resultado = open("data/movimientos.csv","r")
 lectura = resultado.readlines()
 print(lectura)
-"""
+
 import csv
 
 midato= []
@@ -22,3 +22,18 @@ for items in lectura:
     midato.append(items)
 
 print("mi lista: ", midato[0][1])
+
+#ejemplo para registrar datos en csv
+
+import csv
+
+mifichero = open('data/movimientos.csv','a',newline='')
+lectura = csv.writer(mifichero,delimiter=',', quotechar='"')
+lectura.writerow(['24/04/2024','roscon de reyes','-40'])
+
+mifichero.close()
+"""
+
+from datetime import date
+
+print(date.today())
