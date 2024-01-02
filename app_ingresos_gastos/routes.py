d70a2af81f70653376ea2b7df848c6f0e09829c5
LucasTorreg <lucastorregrosa@gmail.com>
2024-01-02 17:04:04 +0100

    cambios en registro con id

--- app_ingresos_gastos/routes.py
+++ app_ingresos_gastos/routes.py
@@ -13,6 +13,7 @@ def index():
     for items in csvReader:
         datos.append(items)
     
+    fichero.close()
 
     return render_template("index.html", data = datos, titulo = "Lista")
 
@@ -24,13 +25,35 @@ def new():
             return render_template("new.html",titulo="Nuevo",tipoAccion="Registro",tipoBoton="Guardar", error = comprobar_error, dataForm = request.form)
 
         else:
+            ##########################Generar el nuevo id para registro#########################################
+            lista_id=[]
+            last_id=""
+            new_id=0
+
+            ficheroId = open('data/last_id.csv','r')
+            #accediendo a cada registro de archivo y darle formato
+            csvReaderId = csv.reader(ficheroId,delimiter=",",quotechar='"')
+            for items in csvReaderId:
+                lista_id.append(items[0])
+            ficheroId.close()
+            last_id = lista_id[len(lista_id)-1]
+            new_id = int(last_id)+1
+            #################################Guardar el id generado en last_id#######################################
+            fichero_new_id=open("data/last_id.csv", "w")
+            fichero_new_id.write(str(new_id))
+            fichero_new_id.close()
+
+
+
+
+            ###################################################################
             #return "esto deberia registrarse " + str(request.form)
             #acceder al archivo y configurar para la carga de un nuevo registro
             mifichero = open('data/movimientos.csv','a',newline='')
             #llamar al método writer de escritura y configuramos formato
             lectura = csv.writer(mifichero,delimiter=',', quotechar='"')
             #registramos los datos recibidos en el archivo csv
-            lectura.writerow([ request.form["fecha"], request.form["concepto"], request.form["monto"] ])
+            lectura.writerow([ new_id, request.form["fecha"], request.form["concepto"], request.form["monto"] ])
             mifichero.close()
 
             return redirect("/")
@@ -39,13 +62,15 @@ def new():
         return render_template("new.html",titulo="Nuevo",tipoAccion="Registro",tipoBoton="Guardar", dataForm = {})
 
 
-@app.route("/delete")
-def delete():
-    return render_template("delete.html", titulo = "Borrar")
+@app.route("/delete/<int:id>")
+def delete(id):
+    return f"El registro a eliminar es el de id:{id}"
+    #return render_template("delete.html", titulo = "Borrar")
 
-@app.route("/update")
-def update():
-    return render_template("update.html", titulo = "Actualizar", tipoAccion = "Actualización", tipoBoton = "Editar", dataForm = {})
+@app.route("/update/<int:id>")
+def update(id):
+    return f"El registro a editar es el de id:{id}"
+    #return render_template("update.html", titulo = "Actualizar", tipoAccion = "Actualización", tipoBoton = "Editar", dataForm = {})
 
 
 
--- pruebas.py
+++ pruebas.py
@@ -34,6 +34,21 @@ lectura.writerow(['24/04/2024','roscon de reyes','-40'])
 mifichero.close()
 """
 
+"""
 from datetime import date
-
+print(type(str(date.today())))
 print(date.today())
+
+"""
+
+"""
+comprobar_error=[1,2,3]@
+if len(comprobar_error) > 0:
+    print("si tiene valores")
+else:
+    print("no tiene valores")    
+"""
+
+lista=[22,11,44,11,66,878,2323,343,67676,888,33]
+
+print(lista[ len(lista)-1 ] )#obtener el ultimo valor de una lista
